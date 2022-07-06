81e07b62a9031e4dd394509574a0adf743c4c592
Igor <igorzhurb@gmail.com>
2022-07-06 16:12:36 +0300

    IntStringCappedMap

--- src/main/java/com/epam/autotasks/collections/IntStringCappedMap.java
+++ src/main/java/com/epam/autotasks/collections/IntStringCappedMap.java
@@ -1,20 +1,15 @@
 package com.epam.autotasks.collections;
 
-import java.util.AbstractMap;
-import java.util.AbstractSet;
-import java.util.ArrayDeque;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Queue;
-import java.util.Set;
+import java.util.*;
 
 class IntStringCappedMap extends AbstractMap<Integer, String> {
 
     private final long capacity;
+    private final Map<Integer, String> map;
 
     public IntStringCappedMap(final long capacity) {
         this.capacity = capacity;
+        map = new LinkedHashMap<>((int) capacity);
     }
 
     public long getCapacity() {
@@ -27,49 +22,71 @@ class IntStringCappedMap extends AbstractMap<Integer, String> {
             @Override
             public Iterator<Entry<Integer, String>> iterator() {
                 return new Iterator<>() {
+                    final Iterator<Entry<Integer, String>> iterator = map.entrySet().iterator();
+
                     @Override
                     public boolean hasNext() {
-                        //implement this method
-                        throw new UnsupportedOperationException();
+                        return iterator.hasNext();
                     }
 
                     @Override
                     public Entry<Integer, String> next() {
-                        //implement this method
-                        throw new UnsupportedOperationException();
+                        return iterator.next();
                     }
                 };
             }
 
             @Override
             public int size() {
-                return IntStringCappedMap.this.size();
+                return map.size();
             }
         };
     }
 
     @Override
     public String get(final Object key) {
-        //implement this method
-        throw new UnsupportedOperationException();
+        return map.get(key);
     }
 
     @Override
     public String put(final Integer key, final String value) {
-        //implement this method
-        throw new UnsupportedOperationException();
+        if (value.length() > capacity) throw new IllegalArgumentException();
+        //Видаляю значення по ключу бо таке ж значення я можу додавати. Потрібно щоб уникнути колізії.
+        String node = map.remove(key);
+
+        while (totalSizeOfValues() + value.length() > capacity)
+            map.remove(entrySet().iterator().next().getKey());
+
+        map.put(key, value);
+        return node;
+    }
+
+    private long totalSizeOfValues() {
+        return map.values().stream().mapToLong(String::length).sum();
     }
 
     @Override
     public String remove(final Object key) {
-        //implement this method
-        throw new UnsupportedOperationException();
+        return map.remove(key);
     }
 
     @Override
     public int size() {
-        //implement this method
-        throw new UnsupportedOperationException();
+        return map.size();
     }
-
 }
+/* public String put(final Integer key, final String value) {
+        if (value.length() > capacity) throw new IllegalArgumentException();
+        // перевірка на той самий ключ
+        String previousValue = null;
+        if (map.containsKey(key)) {
+            previousValue = map.remove(key);
+        }
+        while (totalSizeOfValues() + value.length() > capacity) {
+            map.remove(getFirst(map).getKey());
+        }
+
+        String str = map.put(key, value);
+        if (previousValue != null) return previousValue;
+        return str;
+    }*/
